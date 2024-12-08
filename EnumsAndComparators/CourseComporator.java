d306323531c736557be849136372924d6a1c70f0
BirzhanArtykbay <157303167+BirzhanArtykbay@users.noreply.github.com>
2024-12-08 23:04:23 +0500

    Update CourseComporator.java

--- EnumsAndComparators/CourseComporator.java
+++ EnumsAndComparators/CourseComporator.java
@@ -8,17 +8,14 @@ public class CourseComporator
 	}
 	
 	public int compareByCode(Course c1, Course c2) {
-		// TODO implement me
-		return 0;	
-	}
-	
+   		return c1.getCode().compareTo(c2.getCode());
+}
+
 	public int compareByCredits(Course c1, Course c2) {
-		// TODO implement me
-		return 0;	
-	}
-	
+    		return Integer.compare(c1.getCredits(), c2.getCredits());
+}
+
 	public int compareByEnrollment(Course c1, Course c2) {
-		// TODO implement me
-		return 0;	
-	}
+    		return Integer.compare(c1.getEnrollment(), c2.getEnrollment());
+}
 }
