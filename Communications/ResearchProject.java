a704c4b934ea63c0d838a4c0769cda31b97e3033
asaaaanmyrza <156882836+asaaaanmyrza@users.noreply.github.com>
2024-12-19 00:17:55 +0500

    Update ResearchProject.java
    Нужно доработать

--- Communications/ResearchProject.java
+++ Communications/ResearchProject.java
@@ -1,34 +1,67 @@
-package Communications ;
+package Communications;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ResearchProject {
+
+    private String topic;
+    private List<Researcher> participants; 
+    private List<ResearchPaper> publishedPapers; 
+
+ 
+    public ResearchProject(String topic) {
+        super();
+        this.topic = topic;
+        this.participants = new ArrayList<>();
+        this.publishedPapers = new ArrayList<>();
+    }
+
+ 
+    public void addParticipant(Researcher researcher) {
+        if (researcher != null) {
+            participants.add(researcher);
+        } else {
+            System.out.println("Участник не может быть null.");
+        }
+    }
 
 
+    public void addPaper(ResearchPaper paper) {
+        if (paper != null) {
+            publishedPapers.add(paper);
+        } else {
+            System.out.println("Статья не может быть null.");
+        }
+    }
 
 
-public class ResearchProject
-{
-	
-	private String topic;
-	
-	private List<Researcher> participants;
-	
-	private List<ResearchPaper> publishedPaper;
-	
-	public ResearchProject(String topic) {
-		super();
-		// TODO construct me	
-	}
-	
-	public void addParticipant(Researcher researcher) {
-		// TODO implement me
-		return null;	
-	}
-	
-	public void addPaper(Research Papers parameter) {
-		// TODO implement me
-		return null;	
-	}
-	
-	public List<ResearchPaper> getPaper() {
-		// TODO implement me
-		return null;	
-	}
+    public List<ResearchPaper> getPapers() {
+        return new ArrayList<>(publishedPapers); 
+    }
+
+
+    public String getTopic() {
+        return topic;
+    }
+
+    public void setTopic(String topic) {
+        this.topic = topic;
+    }
+
+    public List<Researcher> getParticipants() {
+        return new ArrayList<>(participants); 
+    }
+
+    public void printProjectInfo() {
+        System.out.println("Тема проекта: " + topic);
+        System.out.println("Участники:");
+        for (Researcher researcher : participants) {
+            System.out.println("- " + researcher.getName());
+        }
+        System.out.println("Опубликованные статьи:");
+        for (ResearchPaper paper : publishedPapers) {
+            System.out.println("- " + paper.getTitle());
+        }
+    }
 }
