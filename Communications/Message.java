dabfdfdf23fea9e5e723e9b657489512fbd9fbf0
asaaaanmyrza <156882836+asaaaanmyrza@users.noreply.github.com>
2024-12-18 15:25:24 +0500

    Update Message.java
    ахахаха

--- Communications/Message.java
+++ Communications/Message.java
@@ -1,63 +1,89 @@
-package Communications ;
+package Communications;
 
+import java.util.Date;
+import EnumsAndComparators.UrgencyLevel;
+import EnumsAndComparators.MessageStatus;
 
+public class Message {
 
+    private Employee sender;
+    private User recipient;
+    private String content;
+    private Date timestamp;
+    private UrgencyLevel level;
+    private MessageStatus status;
+    private String request;
 
-public class Message
-{
-	
-	private Employee sender;
-	
-	private User recipient;
-	
-	private String content;
-	
-	private Date timestamp;
-	
-	private Urgency level urgencyLevel;
-	
-	private Message Status status;
-	
-	private String request;
-	public Message(){
-		super();
-	}
-	
-	public Employee getSender() {
-		// TODO implement me
-		return null;	
-	}
-	
-	public User getRecipient() {
-		// TODO implement me
-		return null;	
-	}
-	
-	public String getContent() {
-		// TODO implement me
-		return "";	
-	}
-	
-	public Date getTimestamp() {
-		// TODO implement me
-		return new Date();	
-	}
-	
-	public Urgency level getLevel() {
-		// TODO implement me
-		return Urgency level.LOW;	
-	}
-	
-	public Message Status getStatus() {
-		// TODO implement me
-		return Message Status.READ;	
-	}
-	
-	public void sendComplaint(Student student, Urgency level level, String complaint_text) {
-		// TODO implement me	
-	}
-	
-	public void sendRequest(Employee sender, Admin dean, String request) {
-		// TODO implement me	
-	}
+    public Message() {
+        super();
+        this.timestamp = new Date();
+        this.status = MessageStatus.UNREAD;  // По умолчанию сообщение не прочитано
+    }
+
+    public Employee getSender() {
+        return sender;
+    }
+
+    public void setSender(Employee sender) {
+        this.sender = sender;
+    }
+
+    public User getRecipient() {
+        return recipient;
+    }
+
+    public void setRecipient(User recipient) {
+        this.recipient = recipient;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public Date getTimestamp() {
+        return timestamp;
+    }
+
+    public UrgencyLevel getLevel() {
+        return level;
+    }
+
+    public void setLevel(UrgencyLevel level) {
+        this.level = level;
+    }
+
+    public MessageStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(MessageStatus status) {
+        this.status = status;
+    }
+
+    public String getRequest() {
+        return request;
+    }
+
+    public void setRequest(String request) {
+        this.request = request;
+    }
+
+    public void sendComplaint(Student student, UrgencyLevel level, String complaintText) {
+        this.setSender(student); 
+        this.setLevel(level);
+        this.setContent(complaintText);
+        this.setTimestamp(new Date());
+    }
+
+    public void sendRequest(Employee sender, Admin dean, String request) {
+        this.setSender(sender);
+        this.setRecipient(dean);
+        this.setRequest(request);
+        this.setTimestamp(new Date());
+        this.setStatus(MessageStatus.UNREAD);
+    }
 }
