9fc6698eae27e3f47d6e82fc8a5e954c24abf194
asaaaanmyrza <156882836+asaaaanmyrza@users.noreply.github.com>
2024-12-19 00:02:39 +0500

    Update Order.java

--- Communications/Order.java
+++ Communications/Order.java
@@ -1,39 +1,65 @@
-package Communications ;
+package Communications;
 
+import java.util.Date;
 
+public class Order {
 
+    private int orderId; 
+    private Date orderDate;
+    private String executer;
+    private String orderInfo;
 
-public class Order
-{
-	
-	private int order_id;
-	
-	private Date orderDate;
-	
-	private String executer;
-	
-	private String orderInfo;
-	public Order(){
-		super();
-	}
-	
-	public int getOrder_id() {
-		// TODO implement me
-		return 0;	
-	}
-	
-	public Date getOrderDate() {
-		// TODO implement me
-		return new Date();	
-	}
-	
-	public String getExecuter() {
-		// TODO implement me
-		return "";	
-	}
-	
-	public String getOrderinfo() {
-		// TODO implement me
-		return "";	
-	}
+    public Order() {
+        super();
+        this.orderDate = new Date();
+    }
+
+    public Order(int orderId, Date orderDate, String executer, String orderInfo) {
+        this.orderId = orderId;
+        this.orderDate = orderDate;
+        this.executer = executer;
+        this.orderInfo = orderInfo;
+    }
+
+    public int getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(int orderId) {
+        this.orderId = orderId;
+    }
+
+    public Date getOrderDate() {
+        return orderDate;
+    }
+
+    public void setOrderDate(Date orderDate) {
+        this.orderDate = orderDate;
+    }
+
+    public String getExecuter() {
+        return executer;
+    }
+
+    public void setExecuter(String executer) {
+        this.executer = executer;
+    }
+
+    public String getOrderInfo() {
+        return orderInfo;
+    }
+
+    public void setOrderInfo(String orderInfo) {
+        this.orderInfo = orderInfo;
+    }
+
+    @Override
+    public String toString() {
+        return "Order{" +
+                "orderId=" + orderId +
+                ", orderDate=" + orderDate +
+                ", executer='" + executer + '\'' +
+                ", orderInfo='" + orderInfo + '\'' +
+                '}';
+    }
 }
