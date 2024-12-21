e25d21ca97a4de1989cf158e7dd5fde0361d9d75
BirzhanArtykbay <157303167+BirzhanArtykbay@users.noreply.github.com>
2024-12-21 23:36:26 +0500

    Update ScheduleEntry.java 1hp

--- Education/ScheduleEntry.java
+++ Education/ScheduleEntry.java
@@ -1,31 +1,58 @@
-package Education ;
+package Education;
+
+import EnumsAndComparators.WeekDay;
+
+public class ScheduleEntry {
+
+    private Lesson lesson;
+    private WeekDay dayLesson;
+    private int room;
+    private String time;
 
 
+    public ScheduleEntry() {
+        super();
+    }
 
+    public ScheduleEntry(Lesson lesson, WeekDay dayLesson, int room, String time) {
+        this.lesson = lesson;
+        this.dayLesson = dayLesson;
+        this.room = room;
+        this.time = time;
+    }
 
-public class ScheduleEntry
-{
-	
-	private Lesson lesson;
-	
-	private WeekDay dayLesson;
-	
-	private int room;
-	
-	private String time;
-	public ScheduleEntry(){
-		super();
-	}
-	
-	public void Schedule() {
-		// TODO implement me	
-	}
-	
-	public void addScheduleEntry() {
-		// TODO implement me	
-	}
-	
-	public void viewSchedule() {
-		// TODO implement me	
-	}
+    public void schedule(Lesson lesson, WeekDay dayLesson, int room, String time) {
+        if (lesson == null || dayLesson == null || time == null || room <= 0) {
+            throw new IllegalArgumentException("Invalid schedule entry details.");
+        }
+        this.lesson = lesson;
+        this.dayLesson = dayLesson;
+        this.room = room;
+        this.time = time;
+
+        System.out.println("Schedule Entry successfully created:");
+        System.out.println(this);
+    }
+
+    public void addScheduleEntry() {
+        System.out.println("Schedule entry for " + lesson.getLessonName() + " on " + dayLesson +
+                " at " + time + " in room " + room + " added to the system.");
+    }
+
+    public void viewSchedule() {
+        if (lesson == null || dayLesson == null || time == null) {
+            System.out.println("No schedule entry available.");
+        } else {
+            System.out.println("Schedule Entry:");
+            System.out.println(this);
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Lesson: " + (lesson != null ? lesson.getLessonName() : "None") +
+                ", Day: " + (dayLesson != null ? dayLesson : "None") +
+                ", Room: " + room +
+                ", Time: " + time;
+    }
 }
