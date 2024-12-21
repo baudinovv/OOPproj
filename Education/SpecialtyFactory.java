bd3b8cf54f0df41a2d5849427466efba50c021eb
BirzhanArtykbay <157303167+BirzhanArtykbay@users.noreply.github.com>
2024-12-21 23:33:40 +0500

    Update SpecialtyFactory.java 1hp

--- Education/SpecialtyFactory.java
+++ Education/SpecialtyFactory.java
@@ -1,16 +1,43 @@
-package Education ;
+package Education;
+
+import EnumsAndComparators.Faculty;
+import java.util.Scanner;
+
+public class SpecialtyFactory {
 
 
+    public SpecialtyFactory() {
+        super();
+    }
 
+   
+    public Specialty createSpecialty() {
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.print("Enter Specialty ID: ");
+        String id = scanner.nextLine();
 
-public class SpecialtyFactory
-{
-	public SpecialtyFactory(){
-		super();
-	}
-	
-	public Specialty createSpecialty() {
-		// TODO implement me
-		return null;	
-	}
+        System.out.print("Enter Specialty Name: ");
+        String name = scanner.nextLine();
+
+        System.out.print("Enter Specialty Description: ");
+        String description = scanner.nextLine();
+
+        System.out.print("Enter Faculty (SITE, SG, BS, ISE, SAM, KMA, SCE): ");
+        Faculty faculty = Faculty.valueOf(scanner.nextLine().toUpperCase());
+
+        System.out.println("Enter Course Details:");
+        System.out.print("Course ID: ");
+        String courseId = scanner.nextLine();
+
+        System.out.print("Course Name: ");
+        String courseName = scanner.nextLine();
+
+        System.out.print("Course Credits: ");
+        int credits = scanner.nextInt();
+
+        Course course = new Course(courseId, courseName, credits, courseName, null, null, null);
+
+        return new Specialty(id, name, course, description, faculty);
+    }
 }
